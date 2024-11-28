929bb52a3ab5d3217710e03bb6b30884423042d2
tw-yoo <twyoo99@gmail.com>
2024-11-28 17:56:38 +0900

    server cors test

--- server_config.sh
+++ server_config.sh
@@ -10,7 +10,7 @@ echo "Current branch: $BRANCH_NAME"
 
 # Define server URLs based on the branch
 if [[ "$BRANCH_NAME" == "main" ]]; then
-  THEWELL_GPT_SERVER_URL="http://thewell-gpt-lb-101888234.ap-northeast-2.elb.amazonaws.com"
+  THEWELL_GPT_SERVER_URL="//thewell-gpt-lb-101888234.ap-northeast-2.elb.amazonaws.com"
 else
   LOCAL_IP=$(ifconfig | grep -E 'inet (192\.|10\.|172\.)' | awk '{print $2}' | head -n 1)
   if [ -z "$LOCAL_IP" ]; then
@@ -25,7 +25,7 @@ fi
 # Echo the Dart code into the file
 cat <<EOF > lib/util/server_config.dart
 String get gptServerUrl {
-  const String defaultUrl = 'http://thewell-gpt-lb-101888234.ap-northeast-2.elb.amazonaws.com';
+  const String defaultUrl = '//thewell-gpt-lb-101888234.ap-northeast-2.elb.amazonaws.com';
 
   const String envUrl = '$THEWELL_GPT_SERVER_URL';
 
